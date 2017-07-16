e7eaf4f1ec75ea4011a3bf7b1ce68738dd66beee
Aitor Riba Archilla <contact@aitorriba.com>
2017-07-16 23:29:27 +0000

    Apply fixes from StyleCI

--- src/Builder.php
+++ src/Builder.php
@@ -11,7 +11,7 @@ class Builder
      *
      * @return string
      */
-    public static function assets($library = NULL)
+    public static function assets($library = null)
     {
         $assets = '';
         if (!$library) {
@@ -22,12 +22,12 @@ class Builder
             dd('v');
             foreach ($library as $key => $libname) {
                 foreach ($libname as $css) {
-                    $assets = $assets . '<link rel="stylesheet" href="' . $css . '" />';
+                    $assets = $assets.'<link rel="stylesheet" href="'.$css.'" />';
                 }
             }
         } else {
             foreach (config('notifier.assets')[$library] as $css) {
-                $assets = $assets . '<link rel="stylesheet" href="' . $css . '" />';
+                $assets = $assets.'<link rel="stylesheet" href="'.$css.'" />';
             }
         }
 
@@ -41,7 +41,7 @@ class Builder
      *
      * @return string
      */
-    public static function notify($string, $type = NULL, $library = NULL)
+    public static function notify($string, $type = null, $library = null)
     {
         if (!$type) {
             $type = config('notifier.defaults.type');
@@ -51,6 +51,6 @@ class Builder
             $library = config('notifier.defaults.library');
         }
 
-        return view('notifier::' . $library, ['string' => is_array($string) ? $string[0] : $string, 'subtitle' => is_array($string) ? $string[1] : '', 'type' => $type]);
+        return view('notifier::'.$library, ['string' => is_array($string) ? $string[0] : $string, 'subtitle' => is_array($string) ? $string[1] : '', 'type' => $type]);
     }
 }
